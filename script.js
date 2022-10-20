895041403aa94e63fc23b3d2943c467e9ccec8bd
Michal-Dudzik <dumichal20@wp.pl>
2022-10-20 23:44:15 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+let btnRight = document.querySelector('#carrouselRight');
+btnRight.addEventListener('click', carrouselRight);
+
+let btnleft = document.querySelector('#carrouselLeft');
+btnleft.addEventListener('click', carrouselLeft);
+
+window.onload = function () {
+	carrouselInit();
+};
+
+function carrouselInit() {
+	var carrousel = document.getElementById('carrouselimg1');
+	carrousel.style.opacity = '1';
+	setInterval(carrouselAutoChange, 5000);
+}
+
+function carrouselAutoChange() {
+	carrouselRight();
+}
+
+function carrouselChange(i) {
+	var carrousel = document.getElementById('carrouselimg' + i);
+	carrousel.style.opacity = '1';
+}
+
+function carrouselLeft() {
+	let nbCarrousel = 5;
+	let num = 0;
+
+	for (let i = 0; i < nbCarrousel; i++) {
+		num = i + 1;
+		var carrousel = document.getElementById('carrouselimg' + num);
+		if (carrousel.style.opacity == '1') {
+			carrousel.style.opacity = '0';
+			if (i == 0) {
+				return carrouselChange(5);
+			}
+			return carrouselChange(num - 1);
+		}
+	}
+}
+
+function carrouselRight() {
+	let nbCarrousel = 5;
+	let num = 0;
+
+	for (let i = 0; i < nbCarrousel; i++) {
+		num = i + 1;
+		var carrousel = document.getElementById('carrouselimg' + num);
+		if (carrousel.style.opacity == '1') {
+			carrousel.style.opacity = '0';
+			if (i == 4) {
+				return carrouselChange(1);
+			}
+			return carrouselChange(num + 1);
+		}
+	}
+}
